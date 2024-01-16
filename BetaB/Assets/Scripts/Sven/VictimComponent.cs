ea29bb2455c5cf6e55aa0cc8a0cdfc9e834dc7b1
Sveeeen4545 <Sven@yard-it.nl>
2024-01-16 18:42:57 +0100

    Testtig script

--- BetaB/Assets/Scripts/Sven/VictimComponent.cs
+++ BetaB/Assets/Scripts/Sven/VictimComponent.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VictimComponent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private SceneData data;
+    private List<Hazard> hazardList;
+    private List<Victim> victimList;
+
+    private void Start()
+    {
+        hazardList = data.hazardList;
+        victimList = data.victimList;
+    }
+
+    public void Testing()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
