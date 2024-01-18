17c49d59bee6490bfee67b962c28fc442a728c50
Sveeeen4545 <Sven@yard-it.nl>
2024-01-18 12:24:40 +0100

    Foreach added

--- BetaB/Assets/Sven/S_OwnData.cs
+++ BetaB/Assets/Sven/S_OwnData.cs
@@ -5,7 +5,12 @@ using TMPro;
 
 public class S_OwnData : MonoBehaviour
 {
-    Victim victim;
+    public Victim victim;
+    public SceneData sceneData;
+
+    public S_VictimDataGrab objectVicitm;
+
+    private List<SceneData> victimList;
 
     public TextMeshProUGUI prioText;
     public TextMeshProUGUI ageText;
@@ -21,6 +26,19 @@ public class S_OwnData : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        foreach (SceneData victim in victimList)
+        {
+            if (victim != null)
+            {
+                Instantiate(victim);
+            }
+        }
+
+
+
+
+
+
         prioText.text = $"Prioroty: {victim.priority}";
         ageText.text = $"Age: {victim.age}";
         hasPulseText.text = $"Has Pulse: {victim.hasPulse}";
