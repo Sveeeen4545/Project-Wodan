8f2e161f61f9a8d6f7145ac07a47b4bcf195a8dd
Sveeeen4545 <Sven@yard-it.nl>
2024-01-20 11:37:14 +0100

    Added the road function

--- BetaB/Assets/Sven/S_CreateRoad.cs
+++ BetaB/Assets/Sven/S_CreateRoad.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class S_CreateRoad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject S_SingleLane;
+    [SerializeField] private GameObject S_DoubleLane;
+    [SerializeField] private GameObject S_CurvedRoad;
+    [SerializeField] private GameObject S_Intersection;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private GameObject RoadPanel;
+
+    public void SpawnSingleLane()
     {
-        
+        Instantiate(S_SingleLane);
+        RoadPanel.SetActive(false);
+    }
+    public void SpawnDoubleLane()
+    {
+        Instantiate (S_DoubleLane);
+        RoadPanel.SetActive(false);
+
+    }
+    public void SpawnCurvedRoad()
+    {
+        Instantiate(S_CurvedRoad);
+        RoadPanel.SetActive(false);
+    }
+    public void SpawnIntersection()
+    {
+        Instantiate(S_Intersection);
+        RoadPanel.SetActive(false);
     }
 }
